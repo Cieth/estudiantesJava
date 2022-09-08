091ed29099a249911eb39b088a5de458e27c504f
Omar Vega <83027913+Cieth@users.noreply.github.com>
2022-09-08 18:06:53 -0500

    Add files via upload

---
+++ Proyecto1/src/project/Estudiante.java
@@ -0,0 +1,37 @@
+package project;
+
+public class Estudiante extends Persona{
+    private String programaAcademico;
+    private double promedio;
+
+
+    public Estudiante() {
+    }
+
+    public Estudiante(int id, String nombre, String programaAcademico, double promedio) {
+        super(id, nombre);
+        this.programaAcademico = programaAcademico;
+        this.promedio = promedio;
+    }
+
+    public String getProgramaAcademico() {
+        return programaAcademico;
+    }
+
+    public void setProgramaAcademico(String programaAcademico) {
+        this.programaAcademico = programaAcademico;
+    }
+
+    public double getPromedio() {
+        return promedio;
+    }
+
+    public void setPromedio(double promedio) {
+        this.promedio = promedio;
+    }
+
+    @Override
+    public String toString() {
+        return "Id:"+ id +"-Nombre: " + nombre + "-Programa: " + programaAcademico +"-Promedio: "+ promedio;
+    }
+}
---
+++ Proyecto1/src/project/Index.java
@@ -0,0 +1,14 @@
+
+package project;
+
+public class Index {
+    
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
+
+

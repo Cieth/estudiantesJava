0de7f181b1cd5dd88de025d647732c89e8a0574d
Omar Vega <83027913+Cieth@users.noreply.github.com>
2022-09-09 19:27:40 -0500

    Update Index.java

--- Proyecto1/src/project/Index.java
+++ Proyecto1/src/project/Index.java
@@ -8,38 +8,96 @@ public class Index {
     
     public static void main(String[] args) {
         
-        Persona vector[] = new Estudiante[4];
+        Estudiante vector[] = new Estudiante[4];
         boolean op = true;
            
         while (op){
-            int option = Integer.parseInt(JOptionPane.showInputDialog(null,"Seleccione una opción: \n"
-                    + "1.Ingresar un estudiante\n"
-                    + "2.Promedio más bajo\n"
-                    + "3.Promedio más alto\n"
-                    + "4.Listado de estudiantes\n"
-                    + "5.Salir"));
-            if(option == 1){
+            int option = Integer.parseInt(JOptionPane.showInputDialog(null, 
+                """
+                Seleccione una opci\u00f3n: 
+                1.Ingresar un estudiante
+                2.Promedio m\u00e1s bajo
+                3.Promedio m\u00e1s alto
+                4.Listado de estudiantes
+                5.Salir"""));
+            if(option == 1)
+            {
+                int c = 0;
                 for(int i = 0; i < vector.length;i++){
-                    if(!Objects.nonNull(vector[i]) ){
+                    if(!Objects.nonNull(vector[i]))
+                    {
                         vector[i] = new Estudiante(
-                        Integer.parseInt(JOptionPane.showInputDialog("")),
-                        JOptionPane.showInputDialog(""),
-                        JOptionPane.showInputDialog(""),
-                        Double.parseDouble(JOptionPane.showInputDialog(""))
-                        );
-                        System.out.println(vector[i]);
+                        Integer.parseInt(JOptionPane.showInputDialog("Ingrese el id")),
+                        JOptionPane.showInputDialog("Ingrese el nombre"),
+                        JOptionPane.showInputDialog("Ingrese la materia"),
+                        Double.parseDouble(JOptionPane.showInputDialog("Ingrese el promedio")));  
                         break;
+                    }else{
+                        c++;
                     }
-                   }
-            }else{
-                System.out.println("HOla");
+                    if(c == 4){
+                        JOptionPane.showMessageDialog(null,"No hay más espacio");     
+                    }
+                }
             }
-
-            
-        }    
+            if(option == 2 )
+            {
+               double min = 99999;
+               int c = 0;
+                for (Estudiante vector1 : vector) {
+                    if (vector1 == null) {
+                        c++;
+                    } else {
+                        if (vector1.getPromedio() < min) {
+                            min = vector1.getPromedio();
+                        }
+                    }
+                }
+                if(c == 4){
+                   JOptionPane.showMessageDialog(null,"No hay estudiantes agregados");     
+                }else{
+                   JOptionPane.showMessageDialog(null,"El menor promedio es: "+min);     
+                }           
+            }    
+            if(option == 3)
+            {
+               double max = -99999;
+               int c = 0;
+                for (Estudiante vector1 : vector) {
+                    if (vector1 == null) {
+                        c++;
+                    } else {
+                        if (vector1.getPromedio() > max) {
+                            max = vector1.getPromedio();
+                        }
+                    }
+                }
+                if(c == 4){
+                   JOptionPane.showMessageDialog(null,"No hay estudiantes agregados");     
+                }else{
+                   JOptionPane.showMessageDialog(null,"El mayor promedio es: "+max);     
+                }           
+            }
+            if(option == 4)
+            {
+                int c = 0;
+                for (Estudiante vector1 : vector) {
+                    if (vector1 == null) {
+                        c++;
+                    }
+                }
+                if(c == 4){
+                   JOptionPane.showMessageDialog(null,"No hay estudiantes agregados");     
+                }else{
+                   JOptionPane.showMessageDialog(null,vector);     
+                }         
+            }
+            if(option <= 0  || option >= 5)
+            {
+                JOptionPane.showMessageDialog(null,"¡Hasta la próxima!");
+                op = false;      
+            }
         }
-            
-        
-
-    }
+    }    
+}
 
